2c44979baccdebc18712be6d818fc4676fe25f3c
Michael Li <michael.li04@sap.com>
2020-05-28 12:23:17 +0800

    RemoveDuplicatsFromSortedArray

---
+++ src/java/com/pplkq/tuto/java/leetcode/RemoveDuplicatsFromSortedArray.java
@@ -0,0 +1,27 @@
+package com.pplkq.tuto.java.leetcode;
+
+import java.util.Arrays;
+
+public class RemoveDuplicatsFromSortedArray {
+
+	public static void main(String[] args) {
+		Integer[] arr = new Integer[] {0,1,1,2,2,3,4,5};
+		
+		System.out.println(Arrays.deepToString(arr));
+		int nl = removeDuplicats(arr);
+		System.out.println(Arrays.deepToString(Arrays.copyOfRange(arr, 0, nl)));
+	}
+
+	private static int removeDuplicats(Integer[] arr) {
+		if(null == arr || arr.length<1) {
+			return -1;
+		}
+		int i=0; 
+		for(int j=1; j<arr.length; j++) {
+			if(arr[i] != arr[j]) {
+				arr[++i] = arr[j];
+			}
+		}
+		return i+1;
+	}
+}
